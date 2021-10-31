b9196c7c96c4fd46976fca28c8cec98d8d3d6ef2
H3 <67812277+HELLLO1073@users.noreply.github.com>
2021-11-01 10:40:32 +1100

    Create SMain.lua

---
+++ SMain.lua
@@ -0,0 +1,9 @@
+if game.PlaceId == 4581966615 then -- anomic
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/ANOMIC.lua", true))()
+    else if game.PlaceId == 4738545896 then -- shooutout
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/Shoot-Destroyer", true))()
+        else if game.PlaceId == 746820961 then -- unit 1968
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLLO1073/RobloxStuff/main/UNIT%3A1968%20GUI%20V4%20(open%20source).lua", true))()            
+        end
+    end   
+end
