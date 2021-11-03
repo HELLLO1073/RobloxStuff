2ab29540e7495235d81057a3e96e734595e8e3e3
H3 <67812277+HELLLO1073@users.noreply.github.com>
2021-11-04 09:36:47 +1100

    Update AnomicInGameTheme.lua

--- AnomicInGameTheme.lua
+++ AnomicInGameTheme.lua
@@ -10,7 +10,7 @@ local colors = {
      stamBar   = Color3.fromRGB(250,20,100),     
 }
 
-function setTheme()
+local function setTheme()
     if LPlayer.PlayerGui:FindFirstChild("MainUIHolder") and LPlayer ~= nil  then 
         LPlayer.PlayerGui.MainMenu.ButtonBar.Teams.BackgroundColor3 = colors.grey
         LPlayer.PlayerGui.MainMenu.ButtonBar.Spawn.BackgroundColor3 = colors.lightGrey
