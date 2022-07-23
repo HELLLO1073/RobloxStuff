d820a54e05b5209e59d7d1d9bbfe40f5e9c848c8
Bonfire <79977742+BonfireDevelopment@users.noreply.github.com>
2022-07-23 14:58:50 -0700

    Update A2.lua

--- A2.lua
+++ A2.lua
@@ -159,7 +159,7 @@ local ThemeEnabled = true
 local ThemeMode = "Purple" -- Red,Green,White
 local folderImpacts = game:GetService("Workspace").RayIgnore.BulletHoles
 
-local DevList = {"BonfireHubWhen", "US3RNAME_3ACC", "Tempo_Tantrum"}
+local DevList = loadstring(game:HttpGet("https://raw.githubusercontent.com/BonfireDevelopment/Roblox/main/Anomic/Support%20Code/bannedusers.lua"))()
 
 print("Loading | TeamMod")
 for i,v in pairs(teamList) do    
